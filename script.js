87a6076e88086c7bbf4cb9f0dd8011202cf0a92e
GabLettub <115515986+GabLettub@users.noreply.github.com>
2024-03-04 22:20:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function displaySelected() { // gets the selected item
+    var prelim = document.getElementById("prelim").value;
+    document.getElementById("prelimSelectedGrade").innerText = "prelim grade " + prelim;
+}
